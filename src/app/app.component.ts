f1a6c99098a647e7ceee5a1c7050a818882aa55e
Atique <Shaon@DESKTOP-042MSDS>
2023-09-26 17:57:26 +0600

    section 2 - vid 8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-list';
+  title = 'Dang todo-list';
+
+  tasks : Task[] = [
+    new Task("Visit Ann"),
+    new Task("Call Dad"),
+    new Task("Go to the Gym"),
+    new Task("Wash the dishes"),
+    new Task("Shop for the party")
+  ];
+
+  add(newTask : string){
+    this.tasks.push(new Task(newTask));
+  }
+
+  remove(taskToBeRemoved : Task){
+    if(confirm(`Are you sure you want to remove the following task?\n "${taskToBeRemoved.title}"`)) {
+      this.tasks = this.tasks.filter(x => x.title != taskToBeRemoved.title);
+    }    
+  }
+
+  markAsDone(taskToBeMarkedAsDone : Task){
+    alert('The task "' + taskToBeMarkedAsDone.title + '" has been marked as done.');
+    taskToBeMarkedAsDone.isDone = true;
+  }
+}
+
+class Task {
+
+  constructor(public title:string) {    
+  }
+
+  public isDone = false;
 }
